9be322f83bab6f1113f4e3c9d80c9aa16a648d01
Vishal Borana <vishal.borana@ad.infosys.com>
2021-03-19 19:04:52 +0530

    Added Update Logged in User Details

--- controllers/auth.js
+++ controllers/auth.js
@@ -62,6 +62,46 @@ exports.getMe = asyncHandler(async (req, res, next) => {
   });
 });
 
+// @desc Update User Details
+// @route PUT /api/v1/auth/updatedetails
+// @access Private
+
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+  const fieldsToUpdate = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+// @desc Update User Password
+// @route PUT /api/v1/auth/updatepassword
+// @access Private
+
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+  // Fetch user by id and select its password which by default is select = false
+  const user = await User.findById(req.user.id).select("+password");
+
+  // Check current Password
+  if (!(await user.matchPassword(req.body.currentPassword))) {
+    return next(new ErrorResponse("Password is incorrect", 401));
+  }
+
+  user.password = req.body.newPassword;
+
+  await user.save();
+
+  sendTokenResponse(user, 200, res);
+});
+
 // @desc Forgot Password
 // @route POST /api/v1/auth/forgotpassword
 // @access Public
